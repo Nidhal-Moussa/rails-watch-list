e3e17e8c6bfc1fb5da30f4e575182ab3ff7be5bc
Nidhal Moussa <sinidhal2001@gmail.com>
2022-09-14 14:20:15 +0200

    managed to create bookmarkts and lists

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,6 +1,5 @@
 class ListsController < ApplicationController
-  before_action :set_params, only: [:show, :destroy]
-
+before_action :find_list, only: [:show]
   def index
     @lists = List.all
   end
@@ -14,24 +13,18 @@ class ListsController < ApplicationController
 
   def create
     @list = List.new(list_params)
-    if @list.save
-      redirect_to list_path(@list)
-    else
-      render :new
-    end
-  end
-
-  def destroy
-    @list.destroy
+    @list.save
+    redirect_to lists_path
   end
 
   private
 
-  def set_params
+  def find_list
     @list = List.find(params[:id])
   end
 
   def list_params
     params.require(:list).permit(:name)
   end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
   resources :lists do
     resources :bookmarks, only: [:new, :create]
   end
-  delete '/bookmarks/:id', to:'bookmarks#destroy'
+  delete '/bookmarks/:id', to: 'bookmarks#destroy'
 end
